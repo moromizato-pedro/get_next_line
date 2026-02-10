e36b49c2f4f4db7a38f07df0d25a24509110d91d
Pedro WSL <moromizatopedro@gmail.com>
2026-02-10 19:11:32 +0000

    Refactor in logic, but still need some refactoring to fit the 25 lines allowed

--- get_next_line.c
+++ get_next_line.c
@@ -6,52 +6,22 @@
 /*   By: pedrohe3 <pedrohe3@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 11:28:00 by pedrohe3          #+#    #+#             */
-/*   Updated: 2026/02/10 16:02:47 by pedrohe3         ###   ########.fr       */
+/*   Updated: 2026/02/10 19:10:23 by pedrohe3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
-	static char	buff[BUFFER_SIZE + 1];
-	char		*next_line;
-	//static char	*next_line;
-	int			i;
-	char		*rest;
-
-	if (fd < 0)
-		return (NULL);
-	next_line = NULL;
-	rest = NULL;
-	i = -1;
-	if (ft_strlen(buff) <= 0)
-		if (read_batch(fd, buff) <= 0)
-			return (NULL);
-	while (buff[++i])
-	{
-		if (buff[i] == '\n')
-		{
-			if (ft_strlen(buff) > 0)
-				ft_strlcat(&next_line, buff, i + 1);
-			ft_strcpy(buff, buff + i + 1);
-			return (next_line);
-		}
-	}
-	ft_strlcat(&next_line, buff, ft_strlen(buff));
-	buff[0] = '\0';
-	//get_next_line(fd);
-	rest = get_next_line(fd);
-	ft_strlcat(&next_line, rest, ft_strlen(rest));
-	return (ft_free(&rest), next_line);
-	//return (next_line);
-}
-/*
 void	putstr(char *buff)
 {
 	int	i;
 
 	i = 0;
+	if (!buff)
+	{
+		printf("(null)\n");
+		return ;
+	}
 	printf("\"");
 	while(1)
 	{
@@ -69,6 +39,42 @@ void	putstr(char *buff)
 	printf("\"\n");
 }
 
+char	*get_next_line(int fd)
+{
+	static char	buff[BUFFER_SIZE + 1];
+	char		*next_line;
+	int			i;
+	int			read_file;
+
+	if (fd < 0)
+		return (NULL);
+	next_line = NULL;
+	// I think I can remove "i" and reset the buff by doing buff - ft_strlen(next_line)
+	i = -1;
+	read_file = 1;
+	while (read_file >= 0)
+	{
+		if (ft_strlen(buff) <= 0)
+			read_file = read_batch(fd, buff);
+		if (read_file == 0)
+			return (next_line);
+		if (buff[++i] == '\n')
+		{
+			if (ft_strlen(buff) > 0)
+				ft_strlcat(&next_line, buff, i + 1);
+			ft_strcpy(buff, buff + i + 1);
+			return (next_line);
+		}
+		else if (buff[i] == '\0')
+		{
+			ft_strlcat(&next_line, buff, ft_strlen(buff));
+			buff[0] = '\0';
+			i = -1;
+		}
+	}
+	return (ft_free(&next_line), NULL);
+}
+/*
 int	main(void)
 {
 
